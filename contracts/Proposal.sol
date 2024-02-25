bcbb8ba96c72b711d7a5cf518d5785f64756149f
supercodernoshadow <dadebonojo@icloud.com>
2024-02-25 15:25:56 -0700

    Add refunding function

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -7,26 +7,31 @@ contract Proposal {
     // Funding deadline constant
     uint256 public constant FUNDING_LEAD = 30 days;
 
-    // Proposal parameters
-    uint256 public startDay; // Date range of the ad, for specified time: start = end
-    uint256 public endDay;
-    uint256 public lat; // Latitude of ad
-    uint256 public long; // Longitude of ad
-    string public target; // Event description
-    string public message; // Proposed message
-    bool public contentType; // 0 - Banner plane, 1 - Skywriter
+	// Proposal parameters
+	uint256 public startDay;			// Date range of the ad, for specified time: start = end
+	uint256 public endDay;
+	uint256 public lat;					// Latitude of ad
+	uint256 public long;				// Longitude of ad
+	string public target;				// Event description
+	string public message;				// Proposed message
+	bool public contentType;			// 0 - Banner plane, 1 - Skywriter
+	
+	uint256 public fundingDeadline;		// Fundraising deadline if fundingTarget not met users can request a refund
+	uint256 public fundingTarget;		// Ad cost 
+	address public provider;			// default provider is the team
+	uint256 public amountFunded;		// current funding total
+	string public url;					// proof of execution
 
     uint256 public fundingDeadline; // Fundraising deadline if fundingTarget not met users can request a refund
     uint256 public fundingTarget; // Ad cost
     address public provider; // default provider is the team
     uint256 public amountFunded; // current funding total
-    string public url; // proof of execution
+    uint256 public contributorCount; // # of contributers
+    bool public proposalAccepted; // tracks proposal status
 
     mapping(address => uint256) private donations; // track donations in event of refunds
 
-    // Event for finalizing proposal - nice to have
-
-    // Proposal Status
+	// Proposal Status
     enum Status {
         Todo,
         Accepted,
@@ -35,6 +40,23 @@ contract Proposal {
 
     // Defaults to first item, e.g., "Todo"
     Status public status;
+
+	constructor(
+		uint256 _startDay,
+		uint256 _endDay,
+		uint256 _lat,
+		uint256 _long,
+		string memory _target,
+		string memory _message,
+		bool _contentType
+	){
+		startDay = _startDay;
+		endDay = _endDay;
+		lat = _lat;
+		long = _long;
+		target = _target;
+		message = _message;
+		contentType = _contentType;
 
     constructor(
         uint256 _startDay,
@@ -68,7 +90,7 @@ contract Proposal {
     }
 
     // Allows users to contribute funds to a proposal if the funding target has not been reached
-    function contribute(uint256 _amount) public payable {
+    function Contribute(uint256 _amount) public payable {
         require(
             amountFunded + _amount <= fundingTarget,
             "cannot contribute more than the funding target"
@@ -79,6 +101,7 @@ contract Proposal {
 
         // Track users donation
         donations[msg.sender] = _amount;
+        contributorCount++;
     }
 
     // Allows contributions by directly sending ether to this contract
@@ -88,20 +111,23 @@ contract Proposal {
 
     // Allows providers to confirm a proposal once the funding target is met
     // Add modifier so that only provider can call this function
-    function acceptProposal() private {
+    function AcceptProposal() private {
         require(
             amountFunded == fundingTarget,
             "proposal has not been fully funded"
         );
         // Update proposal status
-        status = Status.Accepted;
+        proposalAccepted = true;
         // ** Set provider
     }
 
     // Allows user to request a refund after the funding deadline
-    function refund() public {
+    function Refund() public {
         uint256 amount = donations[msg.sender];
-        require(status == Status.Todo, "Proposal has already been accepted");
+        require(
+            proposalAccepted == false,
+            "Proposal has already been accepted"
+        );
         require(amount > 0, "No funds to refund");
         require(
             block.timestamp > fundingDeadline,
@@ -111,12 +137,5 @@ contract Proposal {
         donations[msg.sender] = 0; // Reset the donor's balance before sending to prevent re-entrancy attacks.
         bool sent = payable(msg.sender).send(amount);
         require(sent, "Failed to send Ether");
-    }
-
-    // Complete Proposal by uploading a url to evidence of the ad
-    function completeProposal(string memory _url) private {
-        url = _url;
-        status = Status.Completed;
-        // ** Disburse funds
     }
 }
