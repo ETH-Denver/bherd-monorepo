0c2874d307ed91d2ca4ec797b6131493a69e7635
Evan Tedesco <evantedesco@gmail.com>
2024-02-26 15:24:28 -0700

    Add sepolia connectivity

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@dotenvx/dotenvx').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: "https://eth-sepolia.g.alchemy.com/v2/9PFG-wkAzRM9ATYvxe8g1tb2-tJz67Yq",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    local: {
+      url: "http://127.0.01:8545",
+    },
+  },
   solidity: "0.8.24",
 };
