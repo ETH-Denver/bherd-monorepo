814db3e75d90b001a012b7f4cb56dddec987a5e1
supercodernoshadow <dadebonojo@icloud.com>
2024-03-01 12:15:38 -0700

    merge

--- solidity/contracts/Proposal.sol
+++ solidity/contracts/Proposal.sol
@@ -3,60 +3,42 @@ pragma solidity ^0.8.0;
 
 import "./Deployer.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Proposal is ERC1155 {
-    using Counters for Counters.Counter;
-    Counters.Counter public tokenId;
 
     uint256 public constant FUNDING_LEAD = 30 days;
 
-    struct proposalInfo {
-        address deployer;
-        uint256 startDay;
-        uint256 endDay;
-        uint256 lat;
-        uint256 long;
-        string target;
-        string message;
-        string contentType;
-        uint256 fundingDeadline;
-        uint256 fundingTarget;
-        address provider;
-        uint256 amountFunded;
-    }
-
     // Proposal parameters
-    address public deployer;
+    address public deployer; // Contract factory address
+    address public proposer; // Proposal creator
     uint256 public startDay; // Date range of the ad, for specified time: start = end
-    uint256 public endDay;
-    uint256 public lat; // Latitude of ad
-    uint256 public long; // Longitude of ad
+    int256 public lat; // Latitude of ad
+    int256 public long; // Longitude of ad
     string public target; // Event description
     string public message; // Proposed message
-    string public contentType; // Banner plane, Skywriter etc
+    uint public contentType; // Banner plane, Skywriter etc
 
     uint256 public fundingDeadline; // Fundraising deadline if fundingTarget not met users can request a refund
     uint256 public fundingTarget; // Ad cost
     address public provider; // default provider is the team
     uint256 public amountFunded; // current funding total
     string public url; // proof of execution
+    uint public tokenId; // NFT id
 
     mapping(address => uint256) contributions; // track contributions in event of refunds
 
     constructor(
         address _deployer,
         uint256 _startDay,
-        uint256 _endDay,
-        uint256 _lat,
-        uint256 _long,
+        int256 _lat,
+        int256 _long,
         string memory _target,
-        string memory _contentType,
+        uint _contentType,
         string memory _contentMessage
     ) ERC1155("") {
+        proposer = tx.origin;
         deployer = _deployer;
         startDay = _startDay;
-        endDay = _endDay;
         lat = _lat;
         long = _long;
         target = _target;
@@ -64,10 +46,7 @@ contract Proposal is ERC1155 {
         contentType = _contentType;
 
         // Calculate ad cost based on content type
-        if (
-            keccak256(abi.encodePacked(contentType)) ==
-            keccak256(abi.encodePacked("Skywriting"))
-        ) {
+        if (contentType == 1) {
             fundingTarget = 6e18; // 6 ETH for Skywriter
         } else {
             fundingTarget = 1e18; // 1 ETH for Banner Plane
@@ -80,9 +59,9 @@ contract Proposal is ERC1155 {
             FUNDING_LEAD;
 
         // Mint loyalty token and proposer nft
-        _mint(address(this), tokenId.current(), 1e18, "");
-        tokenId.increment();
-        _mint(tx.origin, tokenId.current(), 1, "");
+        _mint(address(this), tokenId, 1e18, "");
+        tokenId ++;
+        _mint(proposer, tokenId, 1, "");
     }
 
     // Allows users to contribute funds to a proposal if the funding target has not been reached
@@ -109,7 +88,7 @@ contract Proposal is ERC1155 {
     // Add modifier so that only provider can call this function
     function acceptProposal() public {
         require(
-            Deployer(deployer).IsProvider(msg.sender),
+            Deployer(deployer).isProvider(msg.sender),
             "Only providers can accept proposals"
         );
         require(
@@ -134,24 +113,6 @@ contract Proposal is ERC1155 {
         require(sent, "Failed to send Ether");
     }
 
-    function getProposalInfo() public view returns (proposalInfo memory) {
-        proposalInfo memory info = proposalInfo(
-            deployer,
-            startDay,
-            endDay,
-            lat,
-            long,
-            target,
-            message,
-            contentType,
-            fundingDeadline,
-            fundingTarget,
-            provider,
-            amountFunded
-        );
-        return info;
-    }
-
     // Complete Proposal by uploading a url to evidence of the ad
     function completeProposal(string memory _url) public {
         require(
@@ -164,8 +125,8 @@ contract Proposal is ERC1155 {
         require(sent, "Failed to send Ether");
 
         // Mint provider NFT
-        tokenId.increment();
-        _mint(msg.sender, tokenId.current(), 1, "");
+        tokenId ++;
+        _mint(msg.sender, tokenId, 1, "");
     }
 
     function mint() external {
@@ -174,8 +135,8 @@ contract Proposal is ERC1155 {
         require(bytes(url).length != 0, "Proposal is not complete");
 
         // Mint token
-        tokenId.increment();
+        tokenId ++;
         contributions[msg.sender] = 0;
-        _mint(msg.sender, tokenId.current(), 1, "");
+        _mint(msg.sender, tokenId, 1, "");
     }
 }
