bf669351e5478df8f6858772f4dc09f0427c0ba6
Tyler Tedesco <s_tylert@icloud.com>
2024-03-07 14:15:48 -0700

    Remove test typography

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -112,8 +112,7 @@ export const HomePage = () => {
               marginY: 4,
             }}
           >
-            {/* Active Proposals */}
-            test
+            Active Proposals
           </Typography>
           <Container sx={{ minWidth: "100%" }}>{renderProposals()}</Container>
         </Container>
