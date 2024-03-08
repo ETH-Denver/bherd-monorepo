b7e6c31a8fb9825427d0b18eb13af12d8f2f8a4d
Tyler Tedesco <s_tylert@icloud.com>
2024-03-08 12:30:00 -0700

    Set notification state

--- frontend/src/components/layouts/BaseLayout.js
+++ frontend/src/components/layouts/BaseLayout.js
@@ -1,9 +1,46 @@
-import { Container } from "@mui/material";
+import { Alert, Collapse, Container, IconButton } from "@mui/material";
 import { Header } from "components/baseComponents/Header";
-import React from "react";
+import { createContext, useEffect, useState, useMemo, useContext } from "react";
 import { ethDenverTheme } from "../../ethDenverTheme";
+import CloseIcon from "@mui/icons-material/Close";
+
+// Create a context for managing the alert state
+const AlertContext = createContext();
+
+// Custom hook to access the context
+export const useAlertContext = () => {
+  return useContext(AlertContext);
+};
+
+export const AlertProvider = ({ children }) => {
+  // Local state for managing the alert's visibility
+  const [open, setOpen] = useState(true);
+
+  // Retrieve the initial state from local storage or default to true
+  useEffect(() => {
+    const initialOpenState =
+      JSON.parse(localStorage.getItem("alertOpen")) ?? true;
+    setOpen(initialOpenState);
+  }, []);
+
+  // Update local storage whenever the state changes
+  useEffect(() => {
+    localStorage.setItem("alertOpen", JSON.stringify(open));
+  }, [open]);
+
+  // Provide the context value to the children
+  const contextValue = useMemo(() => ({ open, setOpen }), [open, setOpen]);
+
+  return (
+    <AlertContext.Provider value={contextValue}>
+      {children}
+    </AlertContext.Provider>
+  );
+};
 
 export default function BaseLayout({ children }) {
+  const { open, setOpen } = useAlertContext();
+
   return (
     <Container
       disableGutters={true}
@@ -16,6 +53,26 @@ export default function BaseLayout({ children }) {
         flexDirection: "column",
       }}
     >
+      <Collapse in={open}>
+        <Alert
+          severity="warning"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          This app is still in development and is for the purpose of marketing
+          and beta testing.
+        </Alert>
+      </Collapse>
       <Header />
       {children}
       <Container
