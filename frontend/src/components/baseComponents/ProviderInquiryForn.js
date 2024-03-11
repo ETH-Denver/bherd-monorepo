96c614f6bbed59d1578ca12b62196a490cbf5571
Tyler Tedesco <s_tylert@icloud.com>
2024-03-11 12:03:17 -0600

    Set loading component

--- frontend/src/components/baseComponents/ProviderInquiryForn.js
+++ frontend/src/components/baseComponents/ProviderInquiryForn.js
@@ -2,6 +2,7 @@ import { Button, Container, TextField, Typography } from "@mui/material";
 import { ethDenverTheme } from "ethDenverTheme";
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Loader } from "./Loader";
 
 export const ProviderInquiryForm = () => {
   const [firstName, setFirstName] = useState("");
@@ -11,8 +12,11 @@ export const ProviderInquiryForm = () => {
   const [areaOfExpertise, setAreaOfExpertise] = useState("");
   const [company, setCompany] = useState("");
   const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const sendEmail = (e) => {
+  const sendEmail = () => {
+    setLoading(true);
+
     emailjs
       .send(
         "service_bsn21db",
@@ -43,160 +47,168 @@ export const ProviderInquiryForm = () => {
         (error) => {
           console.log(error.text);
         }
-      );
+      )
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
-  return (
-    <Container>
-      <Container
-        id={"2nd-container"}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
-          paddingY: "10vh",
-        }}
-      >
+  if (loading) {
+    return <Loader />;
+  } else {
+    return (
+      <Container>
+        {loading && <Loader />}
         <Container
+          id={"2nd-container"}
           sx={{
-            backgroundColor: ethDenverTheme.palette.secondary.main,
-            paddingY: "4vh",
-            minHeight: "80vh",
-            borderRadius: 2,
-            marginY: 0,
-            overflowY: "scroll",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            paddingY: "10vh",
           }}
         >
-          <Typography
-            variant="h1"
+          <Container
             sx={{
-              fontFamily: "Hanken-Grotesk-Regular",
-              fontWeight: "bold",
-              color: "#fff",
-              marginY: 4,
+              backgroundColor: ethDenverTheme.palette.secondary.main,
+              paddingY: "4vh",
+              minHeight: "80vh",
+              borderRadius: 2,
+              marginY: 0,
+              overflowY: "scroll",
             }}
           >
-            Provider Application
-          </Typography>
-          <Container>
-            <TextField
-              name="firstName"
-              inputProps={{ maxLength: 250 }}
-              label="First Name"
-              required
-              onChange={(e) => setFirstName(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={firstName}
+            <Typography
+              variant="h1"
               sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
+                fontFamily: "Hanken-Grotesk-Regular",
+                fontWeight: "bold",
+                color: "#fff",
+                marginY: 4,
               }}
-            />
-            <TextField
-              inputProps={{ maxLength: 250 }}
-              label="Last Name"
-              required
-              onChange={(e) => setLastName(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={lastName}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
+            >
+              Provider Application
+            </Typography>
+            <Container>
+              <TextField
+                name="firstName"
+                inputProps={{ maxLength: 250 }}
+                label="First Name"
+                required
+                onChange={(e) => setFirstName(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={firstName}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                inputProps={{ maxLength: 250 }}
+                label="Last Name"
+                required
+                onChange={(e) => setLastName(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={lastName}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                inputProps={{ maxLength: 250 }}
+                label="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={email}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                inputProps={{ maxLength: 250 }}
+                label="Phone Number"
+                required
+                onChange={(e) => setPhone(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={phone}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                inputProps={{ maxLength: 250 }}
+                label="Area of Expertise"
+                required
+                onChange={(e) => setAreaOfExpertise(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={areaOfExpertise}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                inputProps={{ maxLength: 250 }}
+                label="Company Site (Optional)"
+                onChange={(e) => setCompany(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={company}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+              <TextField
+                name="description"
+                inputProps={{ maxLength: 250 }}
+                label="Tell us about you and your company"
+                required
+                onChange={(e) => setDescription(e.target.value)}
+                variant="outlined"
+                color="secondary"
+                value={description}
+                sx={{
+                  mb: 3,
+                  width: 500,
+                  backgroundColor: "white",
+                  borderRadius: 2,
+                }}
+              />
+            </Container>
+            <Button
+              variant="contained"
+              onClick={(e) => {
+                sendEmail();
               }}
-            />
-            <TextField
-              inputProps={{ maxLength: 250 }}
-              label="Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={email}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
-              }}
-            />
-            <TextField
-              inputProps={{ maxLength: 250 }}
-              label="Phone Number"
-              required
-              onChange={(e) => setPhone(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={phone}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
-              }}
-            />
-            <TextField
-              inputProps={{ maxLength: 250 }}
-              label="Area of Expertise"
-              required
-              onChange={(e) => setAreaOfExpertise(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={areaOfExpertise}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
-              }}
-            />
-            <TextField
-              inputProps={{ maxLength: 250 }}
-              label="Company Site (Optional)"
-              onChange={(e) => setCompany(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={company}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
-              }}
-            />
-            <TextField
-              name="description"
-              inputProps={{ maxLength: 250 }}
-              label="Tell us about you and your company"
-              required
-              onChange={(e) => setDescription(e.target.value)}
-              variant="outlined"
-              color="secondary"
-              value={description}
-              sx={{
-                mb: 3,
-                width: 500,
-                backgroundColor: "white",
-                borderRadius: 2,
-              }}
-            />
+              sx={{ width: 260, backgroundColor: "#fff", marginRight: "10px" }}
+            >
+              Become A Provider
+            </Button>
           </Container>
-          <Button
-            variant="contained"
-            onClick={(e) => {
-              sendEmail();
-            }}
-            sx={{ width: 260, backgroundColor: "#fff", marginRight: "10px" }}
-          >
-            Become A Provider
-          </Button>
         </Container>
       </Container>
-    </Container>
-  );
+    );
+  }
 };
