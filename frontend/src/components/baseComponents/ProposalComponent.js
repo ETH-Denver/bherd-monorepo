3e37fa49bd9308458e4156f7a22dec869e8e5b22
Tyler Tedesco <s_tylert@icloud.com>
2024-03-20 09:32:10 -0600

    Add styling to proposal component

--- frontend/src/components/baseComponents/ProposalComponent.js
+++ frontend/src/components/baseComponents/ProposalComponent.js
@@ -90,7 +90,6 @@ export const ProposalComponent = ({ proposal }) => {
           boxShadow: "2px 4px 18px rgba(0, 0, 0, 0.3)",
           backgroundImage: "linear-gradient(176deg, #d9d9d9, #cecece 60%)",
           borderRadius: 5,
-          height: "100vh",
           minWidth: "90vw",
           flexDirection: "column",
           alignItems: "center",
@@ -104,7 +103,7 @@ export const ProposalComponent = ({ proposal }) => {
             borderRadius: 5,
             width: "80vw",
             marginY: 5,
-            paddingY: 5,
+            // paddingY: 5,
             boxShadow: "inset 0px 2px 20px 2px rgba(0, 0, 0, 0.1)",
           }}
         >
@@ -113,10 +112,12 @@ export const ProposalComponent = ({ proposal }) => {
               display: "flex",
               alignItems: "center",
               textAlign: "center",
-              height: "300px",
+              height: "330px",
               background: "linear-gradient(to bottom, skyblue, #dff1f8)",
               width: "100%",
               marginBottom: 2,
+              borderTopRightRadius: 21,
+              borderTopLeftRadius: 21,
             }}
           >
             <Typography
@@ -130,13 +131,11 @@ export const ProposalComponent = ({ proposal }) => {
               {message}
             </Typography>
           </Box>
-          <Box sx={{ width: "100%" }}>
-            <Box sx={{ width: "100%", display: "flex" }}>
-              <Typography variant="h4" sx={{ marginX: 2, fontWeight: "bold" }}>
-                Reason for Running:
-              </Typography>
-              <Typography variant="h4">{target}</Typography>
-            </Box>
+          <Box sx={{ width: "100%", display: "flex" }}>
+            <Typography variant="h4" sx={{ marginX: 2, fontWeight: "bold" }}>
+              Reason for Running:
+            </Typography>
+            <Typography variant="h4">{target}</Typography>
           </Box>
           <hr
             style={{
@@ -162,12 +161,25 @@ export const ProposalComponent = ({ proposal }) => {
               marginY: 6,
             }}
           />
-          <Typography
-            variant="h4"
-            sx={{ fontWeight: "bold", marginX: 2, marginBottom: 1 }}
-          >
-            Location:
-          </Typography>
+          <Box sx={{ display: "flex", marginTop: 2 }}>
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                marginX: 2,
+                marginBottom: 1,
+                width: "40%",
+              }}
+            >
+              Location:
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{ fontWeight: "bold", marginX: 2, marginBottom: 1 }}
+            >
+              Details:
+            </Typography>
+          </Box>
           <Box
             sx={{
               display: "flex",
@@ -175,13 +187,11 @@ export const ProposalComponent = ({ proposal }) => {
               width: "100%",
             }}
           >
-            <Box>
-              <MapIndicator
-                lat={Number(lat) / 10 ** 7}
-                long={Number(long) / 10 ** 7}
-                address={address}
-              />
-            </Box>
+            <MapIndicator
+              lat={Number(lat) / 10 ** 7}
+              long={Number(long) / 10 ** 7}
+              address={address}
+            />
             <Box
               sx={{
                 display: "flex",
@@ -262,168 +272,5 @@ export const ProposalComponent = ({ proposal }) => {
         </Box>
       </Container>
     </>
-    // <Container
-    //   sx={{
-    //     backgroundColor: "#fff",
-    //     minHeight: "100vh",
-    //     maxWidth: "50%",
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     alignItems: "center",
-    //     justifyContent: "center",
-    //     fontSize: "calc(10px + 2vmin)",
-    //     color: "black",
-    //   }}
-    // >
-    //   <Stack sx={{ marginBottom: "50px" }} spacing={2} direction="column">
-    //     <Stack spacing={2} direction="row" justifyContent="space-between">
-    //       <Stack>
-    //         <Button sx={{ marginRight: "0px" }} href={"/frontend"}>
-    //           Back to Proposals
-    //         </Button>
-    //       </Stack>
-    //       <Stack spacing={2} direction="row">
-    //         <ProviderAcceptButton fundingStatus={fundingStatus} />
-    //         <ProviderFulfillment url={url} />
-    //         <FundProposalButton
-    //           proposalAddress={proposalAddress}
-    //           amountRemaining={ethers.formatEther(amountFunded)}
-    //           fundingStatus={fundingStatus}
-    //           providerStatus={providerStatus}
-    //         />
-    //         <NFTMintCard
-    //           proposalAddress={proposalAddress}
-    //           fundingStatus={fundingStatus}
-    //           providerStatus={providerStatus}
-    //           isMintingEnabled={isMintingEnabled}
-    //         />
-    //         <ProofOfAddRun
-    //           url={url}
-    //           fundingStatus={fundingStatus}
-    //           providerStatus={providerStatus}
-    //         />
-    //       </Stack>
-    //     </Stack>
-    //     <Box
-    //       sx={{
-    //         display: "flex",
-    //         justifyContent: "center",
-    //         textAlign: "center",
-    //         minHeight: "175px",
-    //         background: "linear-gradient(to bottom, skyblue, #dff1f8)",
-    //         overflow: "hidden",
-    //       }}
-    //     >
-    //       <Typography
-    //         variant="h1"
-    //         sx={{
-    //           fontFamily: "Bubble",
-    //           color: "#fff",
-    //         }}
-    //       >
-    //         {message}
-    //       </Typography>
-    //     </Box>
-    //     <Stack sx={{ marginTop: "40px" }} spacing={2} direction="row">
-    //       <TextField
-    //         disabled
-    //         sx={{ width: "100%" }}
-    //         id="filled-multiline-static"
-    //         label="Intention"
-    //         multiline
-    //         rows={2}
-    //         defaultValue={target}
-    //         variant="filled"
-    //       />
-    //     </Stack>
-    //     <Stack
-    //       sx={{ marginTop: "40px", justifyContent: "space-between" }}
-    //       spacing={2}
-    //       direction="row"
-    //     >
-    //       <TextField
-    //         disabled
-    //         id="filled-multiline-static"
-    //         label="Intention"
-    //         multiline
-    //         defaultValue={formatContentType(contentType)}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Funding Deadline"
-    //         defaultValue={formatDate(fundingDeadline)}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Execution Date"
-    //         defaultValue={formatDate(startDay)}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Funded Amount"
-    //         defaultValue={`ETH ${ethers.formatEther(amountFunded)}`}
-    //         variant="filled"
-    //       />
-    //     </Stack>
-    //     <Stack
-    //       sx={{ marginTop: "40px", justifyContent: "space-between" }}
-    //       spacing={2}
-    //       direction="row"
-    //     >
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Funding Target"
-    //         defaultValue={`ETH ${ethers.formatEther(fundingTarget)}`}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Funding Status"
-    //         defaultValue={fundingStatus}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-disabled"
-    //         label="Provider Status"
-    //         defaultValue={providerStatus}
-    //         variant="filled"
-    //       />
-    //       <TextField
-    //         disabled
-    //         id="filled-multiline-static"
-    //         label="Intention"
-    //         multiline
-    //         defaultValue={address}
-    //         variant="filled"
-    //       />
-    //     </Stack>
-    //     <Stack sx={{ marginTop: "40px" }} spacing={2} direction="row">
-    //       <TextField
-    //         disabled
-    //         sx={{ width: "100%" }}
-    //         id="filled-multiline-static"
-    //         label="Message"
-    //         multiline
-    //         rows={2}
-    //         defaultValue={message}
-    //         variant="filled"
-    //       />
-    //     </Stack>
-    //   </Stack>
-    //   <MapIndicator
-    //     lat={Number(lat) / 10 ** 7}
-    //     long={Number(long) / 10 ** 7}
-    //     address={address}
-    //   />
-    // </Container>
   );
 };
